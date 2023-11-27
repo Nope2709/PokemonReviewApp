eb201d28dfe1bffc4ec15fff0292772d0a2885c0
Nope2709 <duongdhse173027@fpt.edu.vn>
2023-11-27 22:35:58 +0700

    fix models

---
+++ PokemonReviewApp/Models/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Owner
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Gym { get; set; }
+        public Country Country { get; set; }
+    }
+}
---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
